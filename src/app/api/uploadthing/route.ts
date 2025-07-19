f17bf539eeb08912c06441f9927150657702c611
drrajeevkumar73 <abhihomeohall72@gmail.com>
2025-07-19 21:06:47 +0530

    pal

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -5,7 +5,9 @@ import { ourFileRouter } from "./core";
 // Export routes for Next App Router
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
-
+  config: {
+    callbackUrl: "https://autismawareness.in/api/uploadthing", // ✅ explicitly set it
+  },
   // Apply an (optional) custom config:
   // config: { ... },
 });
